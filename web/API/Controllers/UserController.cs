1b08289f6177f60e715869d15a22a3354f23c70b
ikenna charles <46247452+charlykso@users.noreply.github.com>
2022-07-11 17:42:55 +0100

    added phone verification

--- web/API/Controllers/UserController.cs
+++ web/API/Controllers/UserController.cs
@@ -3,25 +3,10 @@ using API.DataAccess;
 using API.Models;
 using API.Repo;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
 {
-<<<<<<< HEAD
-    [Route("api/[controller]")]
-    [ApiController]
-    //[Authorize(Roles = "Admin")]
-    public class UserController : ControllerBase
-    {
-        private readonly IConfiguration? _Iconfig;
-        private readonly IUser? _iUser;
-        public UserController(IUser iUser, IConfiguration Iconfig)
-        {
-            _iUser = iUser;
-            _Iconfig = Iconfig;
-        }
-=======
   [Route("api/[controller]")]
   [ApiController]
   [Authorize(Roles = "Admin")]
@@ -86,7 +71,6 @@ namespace API.Controllers
       }
       catch (System.Exception ex)
       {
->>>>>>> 6b91b90cb96ff0d9cd856e62bb670c665a2654ff
 
         return BadRequest(ex.Message);
       }
@@ -116,81 +100,10 @@ namespace API.Controllers
           _iUser.UpdateUser(Id, user);
           return Ok(user);
         }
-<<<<<<< HEAD
-
-        //api/user/verifyPhone
-        [HttpPost("VerifyPhone")]
-        public ActionResult VerifyUserPhone([FromForm] UserModel newUser)
-        {
-            try
-            {
-                if (ModelState.IsValid)
-                {
-                    var checkPhoneNo = _iUser!.CheckPhone(newUser.Phone_no!);
-                    if (checkPhoneNo == "Not Exist")
-                    {
-                        var verifyNo = new VeriffyPhoneNo(_Iconfig!);
-                        var phoneCode = verifyNo.verifyPhone(newUser.Phone_no!);
-
-                        return Ok(phoneCode);
-                    }else{
-                        return BadRequest("Phone number already exist!");
-                    }
-
-                    
-                }else
-                {
-                    return BadRequest("Please input correct details");
-                }
-                
-            }
-            catch (System.Exception ex)
-            {
-                
-                return BadRequest(ex.Message);
-            }
-        }
-
-        //api/user/createuser
-        [HttpPost("CreateUser")]
-        public ActionResult CreateUser([FromForm] UserModel newUser)
-        {
-            try
-            {
-                if (ModelState.IsValid)
-                {
-                    
-                    var user = new User();
-                    user.Firstname = newUser.Firstname;
-                    user.Lastname = newUser.Lastname;
-                    user.Email = newUser.Email;
-                    user.Phone_no = newUser.Phone_no;
-                    var newUser_password = BCrypt.Net.BCrypt.HashPassword(newUser.Password);
-                    user.Password = newUser_password;
-                    user.Role = newUser.Role;
-                    user.Created_at = DateTime.Now;
-
-                    _iUser!.CreateUser(user);
-                    return Ok(user);
-
-                    
-                }else
-                {
-                    return BadRequest("Please input correct details");
-                }
-                
-            }
-            catch (System.Exception ex)
-            {
-                
-                return BadRequest(ex.Message);
-            }
-=======
         catch (System.Exception ex1)
         {
 
           return BadRequest(ex1.Message);
->>>>>>> 6b91b90cb96ff0d9cd856e62bb670c665a2654ff
         }
       }
       catch (System.Exception ex)
